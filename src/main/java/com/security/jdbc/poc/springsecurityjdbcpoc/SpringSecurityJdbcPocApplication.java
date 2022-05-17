f84da34a39308f270cb9de5ad4e245e71664c9f8
Gayathri-Rajan-Devloper <rajangayathri65@gmail.com>
2022-05-17 14:21:52 +0530

    Spring security with JDBC concept

---
+++ src/main/java/com/security/jdbc/poc/springsecurityjdbcpoc/SpringSecurityJdbcPocApplication.java
@@ -0,0 +1,13 @@
+package com.security.jdbc.poc.springsecurityjdbcpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJdbcPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJdbcPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/jdbc/poc/springsecurityjdbcpoc/SpringSecurityJdbcPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.jdbc.poc.springsecurityjdbcpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJdbcPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
